e753148b1f407a2df5c596e99718176899bce252
Paolo Perego <thesp0nge@gmail.com>
2012-03-16 12:15:10 +0100

    Changed the default return value for read method

--- lib/nexty/config.rb
+++ lib/nexty/config.rb
@@ -5,13 +5,9 @@ module Nexty
   class Config
     include Singleton
 
-    def initialize
-      @default = {"config" => {"host"=>"127.0.0.1", "port"=>"3790", "user"=>"nxadmin", "pass"=>"nxadmin"}}
-    end
-
     def read(file)
       if file.nil? or !File.exists?(file)
-        return @default
+        return {"config" => {"host"=>"127.0.0.1", "port"=>"3790", "user"=>"nxadmin", "pass"=>"nxadmin"}}
       end
       config = YAML.load_file(file)
 
