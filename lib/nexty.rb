c09aefaf2597957d2e189bb159127cc3e4098df2
Paolo Perego <thesp0nge@gmail.com>
2012-03-20 12:51:23 +0100

    Most important news is the Nexty::Report that helps you in create a site list vulnerability export given a Nexpose Connection

--- lib/nexty.rb
+++ lib/nexty.rb
@@ -1,3 +1,4 @@
 require 'nexty/version'
 require 'nexty/connector'
 require 'nexty/site'
+require 'nexty/report'
---
+++ lib/nexty/report.rb
@@ -0,0 +1,31 @@
+module Nexty
+  class Report
+    
+    def self.generate_and_save(site_list, nsc)
+      site_list=Nexty::Sites.load_from_file(site_list)
+      site_list_score = 0
+
+      report = Nexpose::ReportAdHoc.new(nsc, 'report-card', 'csv')
+
+      site_list.each do |site|
+        s = nsc.find_site_by_name_2(site) || []
+        site_config = Nexpose::SiteConfig.new
+        site_config.getSiteConfig(nsc, s[:site_id])
+
+        scan_history = nsc.site_scan_history(s[:site_id])
+        scan_history.sort! { |a,b| b[:start_time] <=> a[:start_time]}
+        scan_history.take(4).each do |scan|
+          report.addFilter('scan', scan[:scan_id])
+        end
+      end
+
+      file_name = "export_#{Time.now.strftime("%Y%m%d%H%M%s")}.csv"
+      file = File.open(file_name, "w")
+      file.write(report.generate)
+      file.close
+
+      file_name
+
+    end
+  end
+end
